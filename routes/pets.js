731b64abd1e199ab0238494ef5faf531d7c73161
LouisPino <LJPino95@gmail.com>
2023-08-10 12:33:58 -0400

    step 9 and 10

--- routes/pets.js
+++ routes/pets.js
@@ -6,6 +6,6 @@ var petCtrl = require('../controllers/pets')
 
 router.get('/', petCtrl.index)
 router.get('/new', petCtrl.newPet)
-router.post('/new', petCtrl.create)
+router.post('/', petCtrl.create)
 
 module.exports = router;
