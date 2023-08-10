ae79492f04ec4748c6df435829292211af04668a
laurhimms <lauriehimmel1@gmail.com>
2023-08-10 09:13:17 -0700

    creating schema and model

--- models/pet.js
+++ models/pet.js
@@ -1,2 +1,16 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+
+const petSchema = new Schema ({
+    species: String,
+    name: String,
+    eyeNo: {
+        type: Number,
+        min: 0
+    },
+    color: String,
+    interests: [String],
+    // ICE BOX: owner:
+})
+
+module.exports = mongoose.model('Pet', petSchema)
